edaac3d272c2f004f323102835bce120284576d1
soo <121608565+soo1e@users.noreply.github.com>
2024-07-04 11:06:05 +0900

    Time: 723 ms (40.62%), Space: 64.7 MB (71.54%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,16 +1,15 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
-
         def backtracking(start, curr):
             if len(curr) == k:
                 result.append(curr[:])
                 return
-
-            for i in range(start, n + 1):  # `n`까지의 숫자 사용
+            
+            for i in range(start, n+1):
                 curr.append(i)
-                backtracking(i + 1, curr)
+                backtracking(i+1, curr)
                 curr.pop()
-
-        backtracking(1, [])  # 숫자는 1부터 시작
+        
+        backtracking(1, [])
         return result
