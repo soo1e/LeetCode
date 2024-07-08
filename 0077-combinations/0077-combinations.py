3388fc10bb762c1ab0b8e28c8e226fc2e1d933e8
soo <121608565+soo1e@users.noreply.github.com>
2024-07-08 15:21:28 +0900

    Time: 749 ms (27.91%), Space: 64.8 MB (40.54%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,15 +1,16 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        result = []
-        def backtracking(start, curr):
+        ans = []
+        def backtrack(start, curr):
             if len(curr) == k:
-                result.append(curr[:])
+                ans.append(curr[:])
                 return
             
-            for i in range(start, n+1):
-                curr.append(i)
-                backtracking(i+1, curr)
-                curr.pop()
+            for num in range(start, n+1):
+                if num not in curr:
+                    curr.append(num)
+                    backtrack(num+1, curr)
+                    curr.pop()
         
-        backtracking(1, [])
-        return result
+        backtrack(1, [])
+        return ans
