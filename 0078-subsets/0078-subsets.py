a96a6bfca952c0b0fc58fe841c0e50f5e6d6c3e3
soo <121608565+soo1e@users.noreply.github.com>
2024-07-12 20:05:26 +0900

    Time: 39 ms (44.06%), Space: 16.7 MB (65.76%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        
-        def backtrack(start, curr):
+
+        def backtracking(start, curr):
+            # 부분집합 -> 모든 요소에 대해서 on/off
+            # 따라서 모든 과정을 append
             ans.append(curr[:])
-            
+
+            # nums의 마지막 인덱스까지 순회
             for i in range(start, len(nums)):
+                # 재귀함수 호출
                 curr.append(nums[i])
-                backtrack(i+1, curr)
+                backtracking(i + 1, curr)
                 curr.pop()
-        
-        backtrack(0, [])
+
+        backtracking(0, [])
         return ans
