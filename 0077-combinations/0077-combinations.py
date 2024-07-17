08d21388226a8234a3c8d109cbc9a363af51c450
soo <121608565+soo1e@users.noreply.github.com>
2024-07-17 16:28:39 +0900

    Time: 681 ms (86.84%), Space: 64.8 MB (42.70%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,15 +1,19 @@
+from typing import List
+
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         ans = []
 
         def backtracking(start, curr):
-            # k와 길이가 같다면 ans에 추가 
+            # k와 길이가 같다면 ans에 추가
             if len(curr) == k:
                 ans.append(curr[:])
                 return
 
             # start부터 n까지 순회
             for i in range(start, n + 1):  # `n`까지의 숫자 사용
+                
                 # 재귀 함수 호출
                 curr.append(i)
                 backtracking(i + 1, curr)
