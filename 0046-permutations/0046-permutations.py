eda5e09caf4659f9a1f032aa1e0093169f6b5f1d
soo <121608565+soo1e@users.noreply.github.com>
2024-07-17 16:21:28 +0900

    Time: 47 ms (16.45%), Space: 16.6 MB (87.22%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,22 +1,21 @@
-# input으로 주어진 nums에 대해서 될 수 있는 모든 순열을 표현하시오
-
-# 
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        
         def backtracking(curr):
+            # base-case : curr의 nums의 길이가 같다면 ans에 현재 만든 순열 추가
             if len(curr) == len(nums):
                 ans.append(curr[:])
                 return
-            
+
+            # list의 수 순서대로 시작 1,2,3...
+            # nums를 순회
             for num in nums:
+                # 현재 접근하는 정수가 curr에 없다면?
                 if num not in curr:
+                    # 재귀함수 호출
                     curr.append(num)
                     backtracking(curr)
                     curr.pop()
-                    
+
         backtracking([])
         return ans
-        
