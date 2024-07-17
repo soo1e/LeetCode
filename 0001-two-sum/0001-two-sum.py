eac0fd3d7de4a2c99d589c170297f6bd34a73d47
soo <121608565+soo1e@users.noreply.github.com>
2024-07-17 16:01:51 +0900

    Time: 6640 ms (5.00%), Space: 18.1 MB (7.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,19 @@
-from typing import List
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # 두 정수를 선택하기 위해 반복문을 2번 돌린다.
+        def backtracking(start, ans):
+            # base-case : ans에 2개의 원소만 포함되어 있다면
+            if len(ans) == 2:
+                # 두 정수의 합이 target과 같다면, ans를 반환
+                if nums[ans[0]] + nums[ans[1]] == target:
+                    return ans
+                return False
+            # start부터 마지막 원소 인덱스까지 순회
+            for i in range(start, len(nums)):
+                ans.append(i)
+                result = backtracking(i + 1, ans)
+                if result:
+                    return result
+                ans.pop()
+            return False
 
-        # i부터 i+1, i+2... 순
-        for i in range(len(nums)):
-            # 동일한 i에 대해 i와 j(i+1), i와 j(i+2)... 순
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        return backtracking(0, [])
