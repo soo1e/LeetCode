f1395f04f7c0b4ffdee7873eb0f4aa7439a35464
soo <121608565+soo1e@users.noreply.github.com>
2024-07-17 16:39:43 +0900

    Time: 186 ms (80.84%), Space: 18.2 MB (27.81%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,14 +1,18 @@
+# 반복문
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
-        
+
+        # left가 right보다 작거나 같을 때까지 반복
         while left <= right:
             mid = left + (right - left) // 2
+            # target = mid면 mid를 반환
             if nums[mid] == target:
                 return mid
+            # target보다 mid가 작으면
             elif nums[mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
-        
+
         return -1
