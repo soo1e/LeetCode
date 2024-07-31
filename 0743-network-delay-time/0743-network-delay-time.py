c979bf2ed0c64cb4cb00850c3be1c69bfc2466ea
soo <121608565+soo1e@users.noreply.github.com>
2024-07-31 16:18:20 +0900

    Time: 394 ms (20.74%), Space: 18.7 MB (33.22%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,32 @@
+import heapq
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # 그래프 구현
+        graph  = defaultdict(list)
+        for time in times:
+            graph[time[0]].append((time[2], time[1]))
+
+        # 다익스트라 알고리즘
+        costs = {}
+        pq = []
+        heapq.heappush(pq, (0, k))
+
+        while pq:
+            cur_cost, cur_node = heapq.heappop(pq)
+            if cur_node not in costs:
+                costs[cur_node] = cur_cost
+                for cost, next_node in graph[cur_node]:
+                    next_cost = cur_cost + cost
+                    heapq.heappush(pq, (next_cost, next_node))
+
+        # 방문 못한 노드 찾기
+        for node in range(1, n+1):
+            if node not in costs:
+                return -1
+
+        # 최소값중에서 최대값 구하기
+        return max(costs.values())
