1321b77eb9717f60797e38fc35a996490be18562
soo <121608565+soo1e@users.noreply.github.com>
2024-08-09 15:51:44 +0900

    Time: 54 ms (85.05%), Space: 17.7 MB (51.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,25 +1,11 @@
-from typing import List
+# 해시 테이블로 풀어보기
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # 원본 리스트의 값과 인덱스를 튜플로 저장
-        indexed_nums = [(num, idx) for idx, num in enumerate(nums)]
-        
-        # 값만 정렬하여 사용하기 위한 리스트
-        sorted_nums = sorted(indexed_nums, key=lambda x: x[0])
-        
-        l, r = 0, len(sorted_nums) - 1
-        
-        while l < r:
-            current_sum = sorted_nums[l][0] + sorted_nums[r][0]
+        list = {}
+        for i, num in enumerate(nums):
+            needed = target - num
+            if needed in list:
+                return [list[needed], i]
             
-            if current_sum < target:
-                l += 1
-            elif current_sum > target:
-                r -= 1
-            else:
-                # 원본 리스트에서의 인덱스를 반환
-                return [sorted_nums[l][1], sorted_nums[r][1]]
-
-        # 솔루션이 보장되지 않는다면 빈 리스트를 반환
-        return []
+            list[num] = i
