760b1571efef515c638d81bdf0bcbacd2559ca88
soo <121608565+soo1e@users.noreply.github.com>
2024-10-14 22:34:30 +0900

    Time: 34 ms (65.82%), Space: 16.5 MB (92.97%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,17 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # 기본 값 세팅
         stack = []
         
-        for x in s:
-            if x == "(":
-                stack.append(")")
-            elif x == "{":
+        # 문자열 s를 문자 단위로 순회 
+        for p in s:
+            # 열린 괄호에 따라 대응되는 닫힌 괄호 추가
+            if p == "{":
                 stack.append("}")
-            elif x == "[":
+            elif p == "[":
                 stack.append("]")
-            elif stack and x == stack[-1]:
+            elif p == "(":
+                stack.append(")")
+            # 현재 문자가 닫힌 괄호 일때, 스택의 마지막 문자가 현재 문자와 같다면, pop!
+            elif stack and stack[-1] == p:
                 stack.pop()
             else:
                 return False
-        
+        # 순회를 종료하고 stack이 비어있다면 True, 아니라면 False
         return not stack
